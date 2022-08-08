ac9df4abc61531830d5d4c32bb6eb79173ec678c
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-08-08 15:23:58 +0100

    changing some layout

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,6 @@
 function FizzBuzz() {
 for (let i = 1; i<= 100; i++) {
-    if (i%5 == 0 && i %3 ==0){
+    if (i%5 == 0 && i%3 == 0){
         console.log('FizzBuzz')
     } else if (i%3 == 0){
         console.log('Fizz')
