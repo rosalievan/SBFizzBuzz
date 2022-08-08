7c058af98c3117906458ff0eeae5593335b9f211
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-08-08 15:37:55 +0100

    first version of bang code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,17 @@
 function FizzBuzz() {
     for (let i = 1; i<= 100; i++) {
+        if (i% 7 == 0){
+            if (i%5 == 0 && i%3 == 0){
+                console.log('FizzBuzzBang')
+            } else if (i%3 == 0){
+                console.log('FizzBang')
+            } else if (i%5 == 0){
+                console.log('BuzzBang')
+            } else {
+                console.log('Bang')
+            }
+
+        }
         if (i%5 == 0 && i%3 == 0){
             console.log('FizzBuzz')
         } else if (i%3 == 0){
